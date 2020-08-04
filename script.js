b720b7eab2e98893641de6a5a1f96dbec966af30
charlesmaillu <charles972@live.com>
2020-08-04 08:42:27 +0300

    add listeners to script.js, object and business logic and user interface

--- script.js
+++ script.js
@@ -1,34 +1,170 @@
-// business logic
-// business logic
-function Contact(first, last) {
-  this.firstName = first;
-  this.lastName = last;
+var price , crust_price, topping_price ;
+let total = 0;
+function Getpizza( name,size,crust,topping, total ){
+  this.name = name;
+  this.size = size;
+  this.crust = crust;
+  this.topping = topping;
+  this.total = total;
 }
 
-Contact.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
-}
+$(document).ready(function(){
+  $("button.proceed").click(function(event){
+   let pname = $(".name option:selected").val();
+   let psize = $("#size option:selected").val();
+   let pcrust = $("#crust option:selected").val();
+   let ptopping = [];
+   $.each($("input[name='toppings']:checked"), function(){            
+       ptopping.push($(this).val());
+   });
+   console.log(ptopping.join(", "));
 
-// user interface logic
-$(document).ready(function() {
-  $("form#new-contact").submit(function(event) {
-    event.preventDefault();
+   switch(psize){
+    case "0":
+      price =0;
+    break;
+    case "large":
+       price = 1400;
+     break;
+     case "medium":
+       price = 950;
+     break;
+     case "small":
+       price = 630;
+     default:
+   }
+   switch(pcrust){
+      case "0":
+        crust_price = 0;
+      break;
+      case "Crispy":
+        crust_price = 200;
+      break;
+      case "Stuffed":
+        crust_price = 250;
+      break;
+      case "Gluten-free":
+        crust_price = 180;
+      break;
+      default:
+    }
+    let topping_value = ptopping.length*50;
+    console.log("toppins value" + topping_value);
 
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
+    if((psize == "0") && (pcrust == "0")){
+      console.log("nothing selected");
+      $("button.proceed").show();
+      $("#information").show();
+      $("div.choise").hide();
+      alert("Please select pizza size and crust"); 
+    }
+    else{
+      $("button.proceed").hide();
+      $("#information").hide();
+      $("div.choise").slideDown(1000);
+    }
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+    total = price + crust_price + topping_value;
+    console.log(total);
+    let checkoutTotal =0;
+    checkoutTotal = checkoutTotal + total;
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+    $("#pizzaname").html($(".name option:selected").val());
+    $("#pizzasize").html( $("#size option:selected").val());
+    $("#pizzacrust").html($("#crust option:selected").val());
+    $("#pizzatopping").html(ptopping.join(", "));
+    $("#totals").html(total);
+    
+// Add pizza button
+    $("button.addPizza").click(function(){
+      let pname = $(".name option:selected").val();
+      let psize = $("#size option:selected").val();
+      let pcrust = $("#crust option:selected").val();
+      let ptopping = [];
+      $.each($("input[name='toppings']:checked"), function(){            
+          ptopping.push($(this).val());
+      });
+      console.log(ptopping.join(", "));
+      switch(psize){
+        case "0":
+          price =0;
+        break;
+        case "large":
+           price = 1200;
+         break;
+         case "medium":
+           price = 850;
+         break;
+         case "small":
+           price = 600;
+         default:
+           console.log("error"); 
+       }
+       switch(pcrust){
+          case "0":
+            crust_price = 0;
+          break;
+          case "Crispy":
+            crust_price = 200;
+          break;
+          case "Stuffed":
+            crust_price = 150;
+          break;
+          case "Gluten-free":
+            crust_price = 180;
+          break;
+          default:
+        }
+        let topping_value = ptopping.length*50;
+        total = price + crust_price + topping_value;
+        checkoutTotal = checkoutTotal + total;
 
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
+      var newOrder = new Getpizza(pname, psize, pcrust,ptopping,total);
+
+      $("#ordersmade").append('<tr><td id="pizzaname">'+newOrder.name +'</td><td id="pizzasize">' + newOrder.size + '</td><td id="pizzacrust">'+newOrder.crust + '</td><td id="pizzatopping">'+newOrder.topping+'</td><td id="totals">'+newOrder.total+'</td></tr>');
+      console.log(newOrder);
+    });
+    $("button#checkout").click(function(){ 
+      $("button#checkout").hide();
+      $("button.addPizza").hide();
+      $("button.deliver").slideDown(1000);
+      $("#addedprice").slideDown(1000);
+      $("#pizzatotal").append(" Hi! <br/>  Your bill is sh. "+checkoutTotal +" pay at the counter.bon appetit! <br/> For home delivery click button below.");
+      alert('Your oder will be ready in 30 minutes')
+      alert("Home delivery will cost Sh 150")
+    });
+    $("button.deliver").click(function(){
+      $(".pizzatable").hide();
+      $(".choise h2").hide();
+      $(".delivery").slideDown(1000);
+      $("#addedprice").hide();
+      $("button.deliver").hide();
+      $("#pizzatotal").hide();
+      let deliceryamount= checkoutTotal+150;
+      $("#totalbill").append("Your bill plus delivery fee is: " + deliceryamount);
+    });
+    $("button#final-order").click(function(event){
+      event.preventDefault();
+      $("#pizzatotal").hide();
+      $(".delivery").hide();
+      $("button#final-order").hide();
+      let deliceryamount= checkoutTotal+150;
+      let person = $("input#name").val();
+      let phone = $("input#phone").val();
+      let location = $("input#location").val();
+
+      if ($("input#name").val() && $("input#phone").val() && $("input#location").val()!=""){
+        $("#finallmessage").append("Shalom " + person +", <br/> thank you for shopping with us. <br/>  your order will be delivered within an hour at "+location+ ". <br/>  on delivery pay Sh. "+deliceryamount +" <br/>  Bon appetit!");
+        $("#totalbill").hide();
+        $("#finallmessage").slideDown(1200);
+        alert('Your oder will be delivered in an hour')
+      }
+      else {
+        alert("Please fill in the details for delivery!");
+        $(".delivery").show();
+        $("button#final-order").show();
+      }
+    });
+   event.preventDefault();
   });
-
-  $(".contact").last().click(function() {
-    $("#show-contact").show();
-    $("#show-contact h2").text(newContact.firstName);
-    $(".first-name").text(newContact.firstName);
-    $(".last-name").text(newContact.lastName);
-  });  
-}); 
+});
